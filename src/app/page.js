e09ba6316d7a15a3b64388d6b6beb47f53e60a9b
RaizerRTX <joshuaignacio39@gmail.com>
2023-09-03 01:47:29 +0800

    New changes

--- src/app/page.js
+++ src/app/page.js
@@ -12,11 +12,8 @@ export default async function Home() {
     <main className="main">
       <div className="container-fluid content_parent_panel">
           <div className="col-12 w-100 col-lg-8 order-2 order-md-3 order-lg-2 mb-4">
-            <div className="card">
-              <div className="row row-bordered g-0">
-                <h3 className="card-header m-0 me-2 page_title"><b>Contents: </b></h3>
-              </div>
-              <div className="container-fluid content_panel overflow-auto">
+            <div className="container">
+              <div className="container-fluid content_panel overflow-auto mt-3">
               {
                 (() => {
                   let container = [];
@@ -28,7 +25,7 @@ export default async function Home() {
                           <h3 className="details_title"><label htmlFor={'collapse_' + index}>{val.title}</label></h3>
                           <div className="summary overflow-auto">
                             <div className="card card-body m-1 img_content overflow-auto" dangerouslySetInnerHTML = {{__html:val.media_content}} />
-                            <div className="card card-body m-1 overflow-auto" dangerouslySetInnerHTML = {{__html:val.text_content}} />
+                            <div className="card card-body m-1 overflow-auto text_content" dangerouslySetInnerHTML = {{__html:val.text_content}} />
                           </div>
                         </div>
                       </>
